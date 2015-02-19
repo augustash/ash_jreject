978ff08ef7daf46211417980e3347db17f8d7037
grafikchaos <josh@grafikchaos.com>
2015-02-19 14:47:01 -0600

    Initial commit of Ash_Jreject module

---
+++ code/Model/System/Config/Source/Browser/Version/Msie.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Use the jQuery Browser Rejection library (jReject) to notify customers
+ * using old browsers to upgrade
+ *
+ * @category    Ash
+ * @package     Ash_Jreject
+ * @copyright   Copyright (c) 2015 August Ash, Inc. (http://www.augustash.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Ash_Jreject_Model_System_Config_Source_Browser_Version_Msie
+{
+    /**
+     * Select options array
+     *
+     * @var array
+     */
+    protected $_options;
+
+    /**
+     * Return a dropdown menu array of values
+     *
+     * @return  array
+     */
+    public function toOptionArray()
+    {
+        $this->_options = array(
+            // array('value' => '', 'label' => Mage::helper('adminhtml')->__('-- Please Select --')),
+            array('value' => 'msie5', 'label' => 'Internet Explorer 5'),
+            array('value' => 'msie6', 'label' => 'Internet Explorer 6'),
+            array('value' => 'msie7', 'label' => 'Internet Explorer 7'),
+            array('value' => 'msie8', 'label' => 'Internet Explorer 8'),
+            array('value' => 'msie9', 'label' => 'Internet Explorer 9'),
+        );
+        return $this->_options;
+    }
+}
---
+++ code/README.md
@@ -0,0 +1,26 @@
+# Ash_Jreject (jQuery Browser Rejection)
+
+### Overview:
+
+ Use the jQuery Browser Rejection library (jReject) to notify customers using old browsers to upgrade or get a better browser.
+
+> jReject is a simple, light-weight library designed to display a popup based on a the browser, specific browser version, specific platforms, or rendering engine. Provides full customization of the popup. Uses a small CSS file, and can easily be used on page load or during a specific page event. Also provides a flexible way to beautifully and cleanly display custom browser alternatives in the popup.
+
+**Requirements**
+
++ jQuery 1.7+
++ [Ash_Jquery](https://github.com/augustash/ash_jquery)
+
+
+### Documentation/Reference Materials:
+
++ http://jreject.turnwheel.com/
+
+
+### External files/directories outside of this module:
+
++ app/etc/modules/Ash_Jreject.xml
++ app/design/frontend/base/default/layout/ash_jreject.xml
++ app/design/frontend/base/default/template/ash_jreject/
++ app/locale/en_US/Ash_Jreject.csv
++ skin/frontend/base/default/ash_jreject
